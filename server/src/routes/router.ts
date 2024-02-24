bd6cc0f8b19a4ba08855bf5a55356e3b1dbc6711
olaleykhan <realjoy9@gmail.com>
2024-02-24 18:15:58 +0100

    init docker

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.use('/lens', (req, res) => res.status(200).json({ message: 'lens route' }));
+router.use('/lens', (req, res) => res.status(200).json({ message: 'lens route change?' }));
 
 export default router;
