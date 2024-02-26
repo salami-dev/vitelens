ae6422cbe1746837a51b2995ea1edf3af0de7041
olaleykhan <realjoy9@gmail.com>
2024-02-26 04:39:18 +0100

    linter

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -29,6 +29,6 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error",
     }
 }
