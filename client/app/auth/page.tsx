89509b2c658059dc0e7edd80a17fb086f29fbf5a
olaleykhan <realjoy9@gmail.com>
2024-03-04 19:45:19 +0100

    aut page

---
+++ client/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FullScreenBG from '../components/FullScreenBG'
+import { Box } from '@mui/material'
+
+const page = () => {
+  return (
+    <Box>
+       <FullScreenBG>
+            <div>Content</div>
+         </FullScreenBG>
+    </Box>
+  )
+}
+
+export default page
