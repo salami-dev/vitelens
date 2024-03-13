af260c9c70e84de9e49b630b0b4a85435af2f312
olaleykhan <realjoy9@gmail.com>
2024-03-13 08:32:24 +0100

    init skeletor of uplaod

--- client/app/upload/page.tsx
+++ client/app/upload/page.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import GoHome from '@/app/components/GoHome'
-import { Box } from '@mui/material'
+import { Box, Container, Typography, Grid } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
 
 const Upload = () => {
   return (
-
-    <>
-    <GoHome/>
-
-    <Box>
-
-        upload images here
+    <Box pt={4} >
+      <Container>
+<Box>
+  <Grid container justifyContent="space-between"   >
+    <Grid item><Typography variant="h5"> Upload Image</Typography></Grid>
+    <Grid item xs={1} >
+      <CloseIcon/>
+    </Grid>
+  </Grid>
+</Box>
+      </Container>
     </Box>
-    </>
   )
 }
 
