eeaab9aaf33ae9f74c39b5fcdf987530265712da
olaleykhan <realjoy9@gmail.com>
2024-03-18 19:55:15 +0100

    ...

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:false,
-    webpack5: true,
     images: {
         domains: ['localhost'], // TODO: add s3 bucket here
     },
